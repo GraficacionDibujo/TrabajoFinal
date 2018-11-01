01ab686ef8004206d797a549d7f691a963b293fc
carlos9808 <eduard9808@hotmail.com>
2018-10-31 23:55:05 -0600

    Holapepe

---
+++ ProyFinalv1/Proy Final v1/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Proy_Final_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cuadro = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Cuadro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cuadro
+            // 
+            this.Cuadro.Image = global::Proy_Final_v1.Properties.Resources.Mickey_Mouse;
+            this.Cuadro.Location = new System.Drawing.Point(2, -1);
+            this.Cuadro.Name = "Cuadro";
+            this.Cuadro.Size = new System.Drawing.Size(1280, 720);
+            this.Cuadro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Cuadro.TabIndex = 0;
+            this.Cuadro.TabStop = false;
+            this.Cuadro.Paint += new System.Windows.Forms.PaintEventHandler(this.Cuadro_Paint);
+            this.Cuadro.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1292, 731);
+            this.Controls.Add(this.Cuadro);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Cuadro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Cuadro;
+    }
+}
+
---
+++ ProyFinalv1/Proy Final v1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Proy_Final_v1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x, y;
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            Console.WriteLine("X: " + x.ToString() + " Y: " + y.ToString());
+        }
+        //Codigo Del Dibujo
+        Pen LapizA = new Pen(Color.Red, 2);
+        SolidBrush BrochaA = new SolidBrush(Color.Brown);
+        SolidBrush BroshaB = new SolidBrush(Color.Blue);
+        Graphics Dibujo; //Objeto Graphics
+
+        private void Cuadro_Paint(object sender, PaintEventArgs e)
+        {
+            Dibujo = e.Graphics; //Para poder dibujar
+            //Lado Izquierdo, Esquina Inferior, Primer rectangulo, lado uno
+            Point A = new Point(99, 718);
+            Point B = new Point(94, 305);
+            //Dibujo.DrawLine(LapizA, p1, p2);
+            Point E = new Point(94, 305);
+            Point F = new Point(204, 312);
+            //Dibujo.DrawLine(LapizA, A, B);
+            Point C = new Point(204, 312);//Lado Izquierdo, Esquina Inferior, Primer rectangulo, lado dos
+            Point D = new Point(209, 493);//Mitad de la linea
+            Point G = new Point(216, 715);
+         
+
+            PointF[] RectanguloII = new PointF[8];
+            RectanguloII[0] = A;
+            RectanguloII[1] = B;
+
+            RectanguloII[2] = E;
+            RectanguloII[3] = F;
+
+            RectanguloII[4] = C;
+            RectanguloII[5] = D;
+            RectanguloII[6] = G;
+            RectanguloII[7] = A;
+
+
+
+
+
+            Dibujo.DrawPolygon(LapizA, RectanguloII);
+            Dibujo.FillPolygon(BrochaA, RectanguloII);
+
+
+            PointF[] RectanguloII2 = new PointF[5];
+            Point[] Arreglo1 =
+            {
+                new Point(),new Point(), new Point(),new Point(), new Point()
+            };
+            Dibujo.DrawPolygon(LapizA, Arreglo1);
+            Dibujo.FillPolygon(BrochaA, Arreglo1);
+
+            Point A1 = new Point(94, 305);
+            Point B1 = new Point(269, 250);
+            Point C1 = new Point(265, 284);
+            Point D1 = new Point(204, 312);
+
+            RectanguloII2[0] = A1;
+            RectanguloII2[1] = B1;
+            RectanguloII2[2] = C1;
+            RectanguloII2[3] = D1;
+            RectanguloII2[4] = A1;
+
+
+
+            //Dibujo.DrawLine(LapizA, A1, B1);
+            //Dibujo.DrawLine(LapizA, B1, C1);
+            //Dibujo.DrawLine(LapizA, C1, D1);
+
+            Dibujo.DrawPolygon(LapizA, RectanguloII2);
+            Dibujo.FillPolygon(BroshaB, RectanguloII2);
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
